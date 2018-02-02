410da099f29143bb7f9366802d6b4ccc22ff9723
Shobhit Pandey <Shobhitpandey8@gmail.com>
2018-02-02 12:47:32 +0530

    added navbar

--- Kryptism/settings.py
+++ Kryptism/settings.py
@@ -104,3 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+
